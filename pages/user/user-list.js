dafe9e069d10021c504a4e9fcaac0d895086a1b9
meanseo <chlalstjok97@gmail.com>
2022-04-11 10:44:27 +0900

    2022-04-11 프로필 추가

--- pages/user/user-list.js
+++ pages/user/user-list.js
@@ -1,35 +1,40 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import tableStyles from "../common/style/table.module.css"
-
-const Table = ({ columns,colspan, data }) => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import tableStyles from '../common/style/table.module.css'
+import Link from 'next/link'
+const Table = ({ columns, colspan, data}) => {
     return (
-        <table className={tableStyles.table}>
+      <table className={tableStyles.table}>
         <thead>
-            {/**<th key={column} className={tableStyles.td}>{column}</th> */}
-            <tr className={tableStyles.tr}>
-                {columns.map((column) => (
-                    <th key={column} className={tableStyles.td}>{column}</th>
+            <tr className={tableStyles.tr}  >
+            {columns.map((column, index) => (
+                  <th className={tableStyles.td} key={index}>{column}</th>
             ))}
-            </tr> 
+          </tr>
         </thead>
         <tbody>
-        { data.length == 0  ?<tr className={tableStyles.tr}>
-                              <td colSpan={colspan} className={tableStyles.td}>데이터가 없습니다</td>
-                              </tr>
-        :data.map((user) => (
-        <tr className={tableStyles.tr}  key={user.username} >
-          <td className={tableStyles.td}>{user.username}</td>
-          <td className={tableStyles.td}>{user.password}</td>
-          <td className={tableStyles.td}>{user.name}</td>
-          <td className={tableStyles.td}>{user.telephone}</td>
-        </tr>
-    ))}
-    
-</tbody>
-</table>
-);
-}
+                { data.length == 0  ?<tr className={tableStyles.tr}>
+                                      <td colSpan={colspan} className={tableStyles.td}>데이터가 없습니다</td>
+                                      </tr>
+                :data.map((user) => (
+                <tr className={tableStyles.tr}  key={user.username} >
+                  <td className={tableStyles.td}>
+                    <Link href={{pathname:`/user/[username]`,
+                                query:{selectedUser: 'test'}}} as={`/user/${user.username}`}>
+                      <a>{user.username}</a>
+                    </Link>
+                    
+                  </td>
+                  <td className={tableStyles.td}>{user.password}</td>
+                  <td className={tableStyles.td}>{user.name}</td>
+                  <td className={tableStyles.td}>{user.telephone}</td>
+                </tr>
+            ))}
+            
+        </tbody>
+      </table>
+    );
+  }
 
 export default function UserList(){
     const columns = ["Username", "Password", "Name", "Telephone"];
